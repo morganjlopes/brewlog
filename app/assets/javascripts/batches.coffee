dcd4247f141dc78a07e15bf67509463d91c458b6
Morgan J Lopes <morganjlopes@gmail.com>
2015-01-28 08:24:49 -0500

    Add name to users and install floatlabel.

--- app/assets/javascripts/batches.coffee
+++ app/assets/javascripts/batches.coffee
@@ -1,3 +1,10 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+jQuery ->
+  $(".dropdown-toggle").dropdown()
+  $('input.floatlabel').floatlabel();
+  $(".toggleable").click ->
+    item = $(this).data("toggle-wrapper")
+    $(item).slideToggle()
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,15 @@ class User < ActiveRecord::Base
   has_many :activities
   has_many :brewery_users
   has_many :breweries, through: :brewery_users
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    name.scan(/\w+/).first
+  end
 end
